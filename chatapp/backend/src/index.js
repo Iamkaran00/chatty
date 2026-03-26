7b179b67d20a7858fc50616b23a30a1133d1cc5e
Karan Sahu <161184262+Iamkaran00@users.noreply.github.com>
2026-03-26 15:17:48 +0530

    Update CORS origin to production URL

--- chatapp/backend/src/index.js
+++ chatapp/backend/src/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ limit: "40mb", extended: true })); // for form-urle
 
 app.use(cookieparser());
 app.use(cors({
-    origin : 'http://localhost:5173',
+    origin :'https://chatapp-7kki.onrender.com',
     credentials:true,
 }));
 app.use('/api/v1/auth',authRoutes);
