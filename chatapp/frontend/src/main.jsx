cd3f5a458a3bee3348c7b29f5e549b750f0b2229
Karan Sahu <161184262+Iamkaran00@users.noreply.github.com>
2026-03-26 19:04:31 +0530

    Update main.jsx

--- chatapp/frontend/src/main.jsx
+++ chatapp/frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
-import "@excalidraw/excalidraw/dist/excalidraw.min.css";
+import "@excalidraw/excalidraw/dist/prod/index.css";
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <StrictMode>
